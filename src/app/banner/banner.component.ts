b38bf9fa994661962356a0cb0e56655096a0c464
pranaykumarsamala <pranaykumarsamala1614@gmail.com>
2023-07-17 20:42:54 +0530

    parent to child comm

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,12 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent {
-bannerData ={
-  heading : "About Our",
-  span: "Company",
-  headingtag: "COLLABORATIVELY BUILD BACKWARD-COMPATIBLE RELATIONSHIPS WHEREAS TACTICAL PARADIGMS.",
-  para:"Take your baking to the next level in 30 days with James Beard Award winner Joanne Chang. During her session, Joanne will help you transform into the baker of your dreams.",
-  imagesrc : '../assets/images/pexels---7810767.jpg',
-  btn: "READ More"
-}
+  bannerData = {
+    heading: "About Our",
+    span: "Company",
+    headingtag: "COLLABORATIVELY BUILD BACKWARD-COMPATIBLE RELATIONSHIPS WHEREAS TACTICAL PARADIGMS.",
+    para: "Take your baking to the next level in 30 days with James Beard Award winner Joanne Chang. During her session, Joanne will help you transform into the baker of your dreams.",
+    imagesrc: '../assets/images/pexels---7810767.jpg',
+    btn: "READ More"
+  };
+
+  design: { p?: string } = {};
+
+  constructor() {
+
+  }
+
+  ngOnChanges() {
+    // console.log('ngOnchanges');
+  }
+
+  ngOnInit() {
+    // console.log('ngOnInt');
+    setTimeout(() => {
+      this.design.p = 'Respond to events in the lifecycle of a component or directive by implementing one or more of the lifecycle hook interfaces in the Angular core library. The hooks give you the opportunity to act on a component or directive instance at the appropriate moment, as Angular creates, updates, or destroys that instance.';
+    }, 2000);
+  }
+
+  ngDoCheck() {
+    // console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    // console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    // console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    // console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    // console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    // console.log('ngOnDestroy');
+  }
 }
--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-design',
   templateUrl: './design.component.html',
   styleUrls: ['./design.component.css']
 })
-export class DesignComponent {
-design ={
-h2:'Design Perfection',
-para: 'We’re a small creative design studio based in New York, working with adventurous clients and providing Creative Concept, Branding, Graphic Design, Art Direction and Digital Communication services.',
-btn: ' Read More'
-}
+export class DesignComponent implements OnChanges, OnInit {
+  @Input() data: any;
+  design = {
+    h2: 'Design Perfection',
+    para: 'We’re a small creative design studio based in New York, working with adventurous clients and providing Creative Concept, Branding, Graphic Design, Art Direction and Digital Communication services.',
+    btn: ' Read More'
+  };
+  constructor() {
+
+  }
+  
+  ngOnChanges(change: SimpleChanges) {
+    console.log(this.data);
+  }
+
+  ngOnInit() {
+    console.log(this.data);
+  }
 }
